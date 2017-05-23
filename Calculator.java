124629ceee77d01a8e1442a2f37047bc5438fee2
Zaykoff <Zaykoff>
2017-05-24 01:07:23 +0300

    user input

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+// класс реализует калькулятор.
+
+public class Calculator{
+	
+	// результат вычисления.
+	private int result;
+	
+	// суммируем аргументы
+	//@param parans аргументы суммирования.
+	public void add(int ... params){
+		for (Integer param : params){
+			this.result += param;
+		}
+	}
+	
+	// получить результат.
+	// @return результат вычисления.
+	public int getResult() {
+		return this.result;
+	}
+	
+	// очистить результат вычисления.
+	public void cleanResult() {
+		this.result = 0;
+	}
+}
